7ace4a93d6cdebc9582d7316132d75b3819b1718
Jorge Espino <jorgeespinonmchs@gmail.com>
2026-09-11 23:17:52 -0400

    NumberTestEvenOrOdd

---
+++ Javascript/NumberTestEvenOrOdd.js
@@ -0,0 +1,12 @@
+function simpleMultiplication(number) {
+    // your code........
+  let test = number * 8
+  message = ''
+  if(number % 2 === 0){
+    newNumber = number * 8
+    return newNumber
+  } else {
+    newNumber = number * 9
+    return newNumber
+  }
+}
